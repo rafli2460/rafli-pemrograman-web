d9b9eec0960a7f6860aa5ca9e7ddf89676b9af27
rafli024 <muhammadrafli2460@gmail.com>
2025-11-12 10:38:51 +0700

    fix kode_kuisxxxx to kode_kuis1070

--- php/uts-1070/delete.php
+++ php/uts-1070/delete.php
@@ -1,8 +1,8 @@
 <?php
 include_once("config.php");
 
-$kode = $_GET['kode_kuisxxxx'];
-$result = mysqli_query($mysqli, "DELETE FROM tb_kuisxxxx WHERE kode_kuisxxxx='$kode'");
+$kode = $_GET['kode_kuis1070'];
+$result = mysqli_query($mysqli, "DELETE FROM tb_kuis1070 WHERE kode_kuis1070='$kode'");
 
 header("Location:index.php");
 ?>
